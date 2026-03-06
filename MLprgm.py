c46db3df027e74d715aa2a96e56b3c60a49ba044
Impana P <impanarangapura@gmail.com>
2026-03-06 14:23:30 +0530

    Added ML training program

---
+++ MLprgm.py
@@ -0,0 +1,13 @@
+# train.py
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import joblib
+
+X = np.array([[1], [2], [3], [4]])
+y = np.array([2, 4, 6, 8])
+
+model = LinearRegression()
+model.fit(X, y)
+
+joblib.dump(model, "model.pkl")
+print("Model trained successfully")
